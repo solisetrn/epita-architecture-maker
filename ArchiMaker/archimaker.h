e885fa056cc855cf1359553cbdb655ea623dd275
Solise Tran <solise.tran@epita.fr>
2026-06-11 22:58:08 +0200

    added: queue management for pretty-printer

--- ArchiMaker/archimaker.h
+++ ArchiMaker/archimaker.h
@@ -8,6 +8,19 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+// pretty_printer.c
+struct node {
+    struct tree *data;
+    struct node *next;
+};
+
+struct queue {
+    struct node *head;
+};
+
+int enqueue(struct queue *q, struct tree *root);
+struct tree *dequeue(struct queue *q);
+
 // tree_manager.c
 enum type {
     DIRECTORY, // == 0
